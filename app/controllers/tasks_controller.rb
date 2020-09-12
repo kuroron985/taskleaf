6cca655018ee198d11990f5703110b0119cca8e1
kuroron985 <shigotoyou.taka@gmil.com>
2020-09-12 12:45:02 +0900

    [add] ストロングパラメータについてのコメントを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
+    task = Task.new(task_params) #←task_params＝private以下のtask_paramsメソッド
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
@@ -23,6 +23,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description) #←DBのtaskモデルからnameとdescriptionの値だけを呼び出す
   end
 end
